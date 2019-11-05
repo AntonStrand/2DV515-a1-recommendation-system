02fec6997a2e3299b307be33ed9c84ea5d728d83
antonstrand <jag@antonstrand.se>
2019-11-05 18:43:35 +0100

    Import data

--- initDB/setupDB.sh
+++ initDB/setupDB.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+# Check for valid commands
+if [ $# -eq 0 ] || ["$1" != "prod" ] || [ "$1" != "dev" ]; then
+  if [ $# -eq 0 ]; then
+     echo "ERROR: No arguments supplied"
+  elif [["$1" != "prod" ] || [ "$1" != "dev" ]; then
+    echo "ERROR: $1 is an invalid argument"
+  fi
+  echo "Valid arguments"
+  echo " prod  for production"
+  echo " dev   for development"
+  exit
+fi
+
 file=../server/.env
 if [ -f "$file" ]; then
-  echo "WARNING: Files already exist are you sure you want to overwrite with new passwords?"
-  read -p "Continue (y/n)?" choice
-  if [[ ! $choice =~ ^[Yy]$ ]]
+  read -p  "WARNING: Files already exist are you sure you want to overwrite with new passwords? [Y/n] " choice
+  if [[ $choice =~ ^[Nn]$ ]]
   then
       echo "Exit"
       exit
   fi
 fi
 
-if [[ $# -eq 0 ]]; then
-  echo "No arguments suplied"
-  echo "prod  for production"
-  echo "dev   for development"
-  exit
-elif [ "$1" == "prod" ]; then
+if [ "$1" == "prod" ]; then
   echo "Creating production credentials."
   NODE_ENV=production
   MYSQL_USERNAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
@@ -32,11 +39,6 @@ elif [ "$1" == 'dev' ]; then
   ROOT=root
   ROOT_PASSWORD=password
   DATABASE=dev
-else
-  echo "Unknown parameter"
-  echo "prod  for production"
-  echo "dev   for development"
-  exit
 fi
 
 # Create .env settings
@@ -47,7 +49,7 @@ NODE_ENV=$NODE_ENV
 
 # MySQL
 MYSQL_VERSION=5.7.22
-MYSQL_HOST=movie-mysql
+MYSQL_HOST=db
 MYSQL_DATABASE=$DATABASE
 MYSQL_ROOT_USER=$ROOT
 MYSQL_ROOT_PASSWORD=$ROOT_PASSWORD
@@ -57,4 +59,73 @@ EOF
 
 echo "Start with: docker-compose up"
 cd ..
-docker-compose up
+docker-compose up -d
+
+# Allow docker to start
+sleep 10
+
+# Adding tables to database
+docker exec -i db mysql -u${ROOT} -p${ROOT_PASSWORD} <<MYSQL
+use $DATABASE;
+CREATE TABLE movies(
+movie_id INT NOT NULL,
+title VARCHAR(100) NOT NULL,
+year INT NOT NULL,
+PRIMARY KEY (movie_id)
+); 
+CREATE TABLE users(
+user_id INT NOT NULL,
+name VARCHAR(100) NOT NULL,
+PRIMARY KEY (user_id)
+); 
+CREATE TABLE ratings(
+user_id INT NOT NULL,
+movie_id INT NOT NULL,
+rating FLOAT(2,1) NOT NULL,
+PRIMARY KEY (user_id, movie_id)
+); 
+MYSQL
+
+function insertMovies() {
+  echo "Inserting movies..."
+  # Remove headline
+  cat ./initDB/$1/movies.csv | sed 1d > temp.csv
+  cat temp.csv
+  # Loop through each line
+  cat temp.csv | while IFS=$';' read movie_id title year
+  do
+    echo "INSERT IGNORE INTO movies (movie_id, title, year) VALUES ($movie_id, $title, $year);"
+  done | docker exec -i db mysql -u${ROOT} -p${ROOT_PASSWORD} ${DATABASE}
+  echo "Movies inserted"
+  rm temp.csv
+}
+
+function insertUsers() {
+  echo "Inserting users..."
+  # Remove headline
+  cat ./initDB/$1/users.csv | sed 1d > temp.csv
+  # Loop through each line
+  cat temp.csv | while IFS=$';' read user_id name
+  do
+    echo "INSERT IGNORE INTO users (user_id, name) VALUES ($user_id, '$name');"
+  done | docker exec -i db mysql -u${ROOT} -p${ROOT_PASSWORD} ${DATABASE}
+  echo "Users inserted"
+  rm temp.csv
+}
+
+function insertRatings() {
+  echo "Inserting ratings..."
+  # Remove headline
+  cat ./initDB/$1/ratings.csv | sed 1d > temp.csv
+  # Loop through each line
+  cat temp.csv | while IFS=$';' read user_id movie_id rating
+  do
+    echo "INSERT IGNORE INTO ratings (user_id, movie_id, rating) VALUES ($user_id, $movie_id, $rating);"
+  done | docker exec -i db mysql -u${ROOT} -p${ROOT_PASSWORD} ${DATABASE}
+  echo "Ratings inserted"
+  rm temp.csv
+}
+
+insertMovies $2
+insertUsers $2
+insertRatings $2     
