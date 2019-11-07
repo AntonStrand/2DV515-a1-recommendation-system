2abff5097b8a2844b2ea7acc1a27b2f089db50dc
Anton Strand <jag@antonstrand.se>
2019-11-08 00:47:14 +0100

    Verify that the path to the dataset is valid

--- scripts/populate-db.sh
+++ scripts/populate-db.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-path=$1
-env=$([ -z "$2" ] && echo "../server/.env" || echo $2)
+DATASET_PATH=$1
+ENV=${2:-"../server/.env"}
+CONTAINER=${3:-"db"}
 
 if [ $# -eq 0 ]; then
   echo "ERROR: No arguments supplied"
   echo "Requires: path to the dataset folder"
   echo "Optional: path to .env (default: ../server/.env)"
-  exit;
+  exit 1;
 fi
 
-if [ ! -f "$env" ]; then
+# Verify that the provided dataset directory exists and contains all the files.
+if [[ ! -d $DATASET_PATH ]]; then echo "Directory \"$DATASET_PATH\" does not exists."; exit 1; fi
+if [[ ! -f "$DATASET_PATH/movies.csv" ]]; then echo "Directory \"$DATASET_PATH\" does not contain \"movies.csv\"."; exit 1; fi
+if [[ ! -f "$DATASET_PATH/ratings.csv" ]]; then echo "Directory \"$DATASET_PATH\" does not contain \"ratings.csv\"."; exit 1; fi
+if [[ ! -f "$DATASET_PATH/users.csv" ]]; then echo "Directory \"$DATASET_PATH\" does not contain \"users.csv\"."; exit 1; fi
+
+if [ ! -f "$ENV" ]; then
   echo "The .env file could not be found."
   exit
 fi
 
 function getEnv() {
-  grep "$1" $env | cut -d '=' -f2 | sed -n 1p
+  grep "$1" $ENV | cut -d '=' -f2 | head -n1
 }
 
 # Get env variables
@@ -25,11 +32,12 @@ ROOT=$(getEnv ROOT)
 DATABASE=$(getEnv DATABASE)
 
 # Check that Docker is running
-if [ docker exec -i db mysql -u${ROOT} -p${ROOT_PASSWORD} -e "select 1" > /dev/null 2>&1 ]; then echo You need to start Docker first; exit; fi
+IS_DOCKER_RUNNING=$(docker inspect --format="{{.State.Running}}" $CONTAINER 2> /dev/null)
+if [[ "$IS_DOCKER_RUNNING" == "false" ]]; then echo "You need to start Docker first"; exit 1; fi
 
 echo "Inserting movies..."
 # Format data
-  cat $path/movies.csv | sed 1d | sed 's/,/;/g' | sed 's/"//g' | cut -d";" -f1,2 > temp.csv
+  cat $DATASET_PATH/movies.csv | sed 1d | sed 's/,/;/g' | sed 's/"//g' | cut -d";" -f1,2 > temp.csv
 # Loop through each line
 cat temp.csv | while IFS=$';' read movie_id title 
 do
@@ -40,7 +48,7 @@ rm temp.csv
 
 echo "Inserting users..."
 # Format data
-cat $path/users.csv | sed 1d | sed 's/,/;/g' | sed 's/"//g' | cut -d";" -f1,2 > temp.csv
+cat $DATASET_PATH/users.csv | sed 1d | sed 's/,/;/g' | sed 's/"//g' | cut -d";" -f1,2 > temp.csv
 # Loop through each line
 cat temp.csv | while IFS=$';' read user_id name
 do
@@ -51,7 +59,7 @@ rm temp.csv
 
 echo "Inserting ratings..."
 # Format data
-cat $path/ratings.csv | sed 1d | sed 's/,/;/g' | sed 's/"//g' | cut -d";" -f1,2,3 > temp.csv
+cat $DATASET_PATH/ratings.csv | sed 1d | sed 's/,/;/g' | sed 's/"//g' | cut -d";" -f1,2,3 > temp.csv
 # Loop through each line
 cat temp.csv | while IFS=$';' read user_id movie_id rating
 do
