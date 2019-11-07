0eaff03c3586b0e67473fac106f014b0c712409c
antonstrand <jag@antonstrand.se>
2019-11-07 12:22:58 +0100

    Server responds with dummy data

--- server/app.js
+++ server/app.js
@@ -1,1 +1,35 @@
-console.log("Hello from server")
+console.log('Hello from server')
+require('dotenv').config()
+const http = require('http')
+const { mount, logger, routes, methods, json } = require('paperplane')
+
+// const middleware = [
+//   require('redux-future2'),
+//   require('./src/utils/internalErrorMiddleware')
+// ]
+
+// const app = require('./src/routes')
+
+const app = routes({
+  '/': methods({
+    GET: () =>
+      json({
+        message: 'Welcome to server'
+      })
+  })
+})
+// '/users': methods({
+//   GET: request => users.getUsers(request).map(format(request))
+// }),
+// '/users/:id': methods({
+//   GET: request => users.getUser(request).map(format(request))
+
+const port = process.env.PORT || 3001
+
+http
+  .createServer(mount({ app, logger }))
+  .listen(port, () =>
+    console.log(
+      '\nServer is running on port ' + port + '\nPress ctrl+c to terminate...\n'
+    )
+  )
