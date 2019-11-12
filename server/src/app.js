f010dd7bd89250158e713ac6fb7fb76510d286d2
antonstrand <jag@antonstrand.se>
2019-11-12 17:04:56 +0100

    Implement recommended users & movies by Pearson

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const http = require('http')
 const { mount, logger, routes, methods, json } = require('paperplane')
 const mysql = require('./config/mysql')
-const { groupBy, isEmpty, pLift, roundDec2, desc } = require('./lib/helpers')
+const { groupBy, isEmpty, pLift, desc } = require('./lib/helpers')
 
 const getUsersQuery = `SELECT * FROM users; `
 
@@ -58,7 +58,30 @@ const euclidean = (as, bs) => {
   return sim > 0 ? 1 / (1 + sim) : 0
 }
 
-const sortedEuclidean = (user, rest) =>
+const pearson = (as, bs) => {
+  let sumA = (sumB = sumAsq = sumBsq = pSum = n = 0)
+
+  as.forEach(a => {
+    bs.forEach(b => {
+      if (a.movie_id === b.movie_id) {
+        sumA += a.rating
+        sumB += b.rating
+        sumAsq += a.rating ** 2
+        sumBsq += b.rating ** 2
+        pSum += a.rating * b.rating
+        n++
+      }
+    })
+  })
+
+  if (n == 0) return 0
+
+  const num = pSum - (sumA * sumB) / n
+  const den = Math.sqrt((sumAsq - sumA ** 2 / n) * (sumBsq - sumB ** 2 / n))
+  return num / den
+}
+
+const sortedMetric = metricFn => (user, rest) =>
   groupBy('user_id')(rest)
     .reduce(
       (users, u) =>
@@ -67,7 +90,7 @@ const sortedEuclidean = (user, rest) =>
           : users.concat({
             user_id: u[0].user_id,
             name: u[0].name,
-            simularity: roundDec2(euclidean(user, u))
+            simularity: metricFn(user, u)
           }),
       []
     )
@@ -75,7 +98,14 @@ const sortedEuclidean = (user, rest) =>
 
 /** findTopMatchingEqulideanUsers :: Number -> [ SimularityData ] */
 const findTopMatchingEqulideanUsers = id =>
-  pLift(sortedEuclidean)(
+  pLift(sortedMetric(euclidean))(
+    query(ratedMovies, [id]),
+    query(getWhoAlsoHaveRatedSameMovies, [id, id])
+  )
+
+/** findTopMatchingPearsonUsers :: Number -> [ SimularityData ] */
+const findTopMatchingPearsonUsers = id =>
+  pLift(sortedMetric(pearson))(
     query(ratedMovies, [id]),
     query(getWhoAlsoHaveRatedSameMovies, [id, id])
   )
@@ -83,29 +113,38 @@ const findTopMatchingEqulideanUsers = id =>
 const findMovie = (users, notSeenMovies) =>
   Object.values(
     notSeenMovies.reduce((table, { movie_id, ...m }) => {
-      let sim = roundDec2(users.find(u => u.user_id === m.user_id).simularity)
+      let sim = users.find(u => u.user_id === m.user_id).simularity
       const tm = table[movie_id] || { ws: 0, sim: 0 }
-      return {
-        ...table,
-        [movie_id]: {
-          movie_id,
-          title: m.title,
-          ws: roundDec2(tm.ws + m.rating * sim),
-          sim: roundDec2(tm.sim + sim)
+      // Guard against 0 or negative simularities
+      return sim <= 0
+        ? table
+        : {
+          ...table,
+          [movie_id]: {
+            movie_id,
+            title: m.title,
+            ws: tm.ws + m.rating * sim,
+            sim: tm.sim + sim
+          }
         }
-      }
     }, {})
   )
     .map(({ title, movie_id, ws, sim }) => ({
       title,
       movie_id,
-      score: roundDec2(ws / sim)
+      score: ws / sim
     }))
     .sort(desc('score'))
 
 const findTopEqulideanMovies = id =>
   pLift(findMovie)(
     findTopMatchingEqulideanUsers(id),
+    query(getNotSeenMovies, [id])
+  )
+
+const findTopPearsonMovies = id =>
+  pLift(findMovie)(
+    findTopMatchingPearsonUsers(id),
     query(getNotSeenMovies, [id])
   )
 
@@ -116,6 +155,9 @@ const app = routes({
   '/users/:id': methods({
     GET: ({ params: { id } }) => findTopEqulideanMovies(id).then(json)
     // findTopMatchingEqulideanUsers(id).then(json)
+  }),
+  '/pearson/:id': methods({
+    GET: ({ params: { id } }) => findTopPearsonMovies(id).then(json)
   })
 })
 
