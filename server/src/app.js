2537966a9735782769c97a67d244912e722b3fde
Anton Strand <jag@antonstrand.se>
2019-11-13 22:33:46 +0100

    Adding endpoint for getting available metrics

--- server/src/app.js
+++ server/src/app.js
@@ -2,19 +2,24 @@ require('dotenv').config()
 const http = require('http')
 const { mount, logger, routes, methods, json } = require('paperplane')
 
-const { allUsers } = require('./db/queries')
-const { euclidean, pearson } = require('./models/metrics')
+const { getAllUsers } = require('./db/queries')
+const metrics = require('./models/metrics')
 const findTopMoviesBy = require('./models/topMovies')
 
 const app = routes({
   '/users': methods({
-    GET: () => query(allUsers).then(json)
+    GET: () => getAllUsers().then(json)
   }),
   '/users/:id': methods({
-    GET: ({ params: { id } }) => findTopMoviesBy(euclidean, id).then(json)
+    GET: ({ params: { id } }) =>
+      findTopMoviesBy(metrics.euclidean, id).then(json)
   }),
   '/pearson/:id': methods({
-    GET: ({ params: { id } }) => findTopMoviesBy(pearson, id).then(json)
+    GET: ({ params: { id } }) =>
+      findTopMoviesBy(metrics.pearson, id).then(json)
+  }),
+  '/metrics': methods({
+    GET: () => json(Object.keys(metrics))
   })
 })
 
