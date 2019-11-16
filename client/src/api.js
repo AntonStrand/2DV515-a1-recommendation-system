66a691a233d147b2fdb04764cf232001cbdbe2d2
Anton Strand <jag@antonstrand.se>
2019-11-16 16:53:27 +0100

    export get

--- client/src/api.js
+++ client/src/api.js
@@ -5,7 +5,8 @@ import { API_BASE_URL } from './config'
 const fGet = encaseP(axios.get)
 
 /** get :: String -> Future a */
-const get = path => fGet(`${API_BASE_URL}${path}`).map(result => result.data)
+export const get = path =>
+  fGet(`${API_BASE_URL}${path}`).map(result => result.data)
 
 /** getUsers :: Future [User] */
 export const getUsers = get('/users')
