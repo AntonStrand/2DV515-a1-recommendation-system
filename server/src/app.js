6ff5dd3c9341e01350c80f44f5aa55fecd178ef0
Anton Strand <jag@antonstrand.se>
2019-11-17 21:50:05 +0100

    Refactor

--- server/src/app.js
+++ server/src/app.js
@@ -1,37 +1,7 @@
 require('dotenv').config()
 const http = require('http')
-const { cors, mount, logger, routes, methods, json } = require('paperplane')
-
-const { getAllUsers } = require('./db/queries')
-const metrics = require('./models/metrics')
-const findTopMoviesBy = require('./models/topMovies')
-const findTopMatchingUsersBy = require('./models/similarUsers')
-
-const { take } = require('./lib/helpers')
-
-const app = cors(
-  routes({
-    '/users': methods({
-      GET: () => getAllUsers().then(json)
-    }),
-    '/users/:id': methods({
-      GET: ({ params, query }) =>
-        findTopMatchingUsersBy(metrics[query.metric], params.id)
-          .then(take(query.limit))
-          .then(json)
-    }),
-    '/movies/:id': methods({
-      GET: ({ params, query }) =>
-        findTopMoviesBy(metrics[query.metric], params.id)
-          .then(take(query.limit))
-          .then(json)
-    }),
-    '/metrics': methods({
-      GET: () => json(Object.keys(metrics))
-    })
-  })
-)
-
+const { mount, logger } = require('paperplane')
+const app = require('./routes')
 const port = process.env.PORT || 3001
 
 http
---
+++ server/src/routes/index.js
@@ -0,0 +1,46 @@
+const { cors, routes, methods, json } = require('paperplane')
+
+const { getAllUsers } = require('../db/queries')
+const metrics = require('../models/metrics')
+const findTopMoviesBy = require('../models/topMovies')
+const findTopMoviesFor = require('../models/topItemBasedMovies')
+const findTopMatchingUsersBy = require('../models/similarUsers')
+
+const { take } = require('../lib/helpers')
+
+const endpoints = {
+  '/users': methods({
+    GET: () => getAllUsers().then(json)
+  }),
+  '/users/:id': methods({
+    GET: ({ params, query }) =>
+      findTopMatchingUsersBy(metrics[query.metric], params.id)
+        .then(take(query.limit))
+        .then(json)
+  }),
+  '/movies/:id': methods({
+    GET: ({ params, query }) =>
+      findTopMoviesBy(metrics[query.metric], params.id)
+        .then(take(query.limit))
+        .then(json)
+  }),
+  '/metrics': methods({
+    GET: () => json(Object.keys(metrics))
+  }),
+  '/item-based/:id': methods({
+    GET: ({ params }) => findTopMoviesFor(params.id).then(json)
+  })
+}
+
+module.exports = cors(
+  routes({
+    '/': methods({
+      GET: () =>
+        json({
+          description: 'available routes',
+          routes: Object.keys(endpoints)
+        })
+    }),
+    ...endpoints
+  })
+)
