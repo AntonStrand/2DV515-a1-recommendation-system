b20e1c912171933df3dfc580ae55d9ef5b54a216
Anton Strand <jag@antonstrand.se>
2019-11-17 10:52:35 +0100

    Reduce duplication

--- client/src/components/RecommendationTable.js
+++ client/src/components/RecommendationTable.js
@@ -3,54 +3,48 @@ import { Table, Card, Heading } from 'evergreen-ui'
 
 const Message = msg => () => <Heading align='center'>{msg}</Heading>
 
-const MovieTable = movies => (
-  <Fragment>
+const TableTemplate = ({ heading, headerText, rows }) => (
+  <>
     <Heading align='center' marginBottom={16}>
-      Movies
+      {heading}
     </Heading>
     <Table>
       <Table.Head>
-        <Table.TextHeaderCell>Title</Table.TextHeaderCell>
-        <Table.TextHeaderCell>ID</Table.TextHeaderCell>
-        <Table.TextHeaderCell>Score</Table.TextHeaderCell>
+        <Table.TextHeaderCell>{headerText[0]}</Table.TextHeaderCell>
+        <Table.TextHeaderCell>{headerText[1]}</Table.TextHeaderCell>
+        <Table.TextHeaderCell>{headerText[2]}</Table.TextHeaderCell>
       </Table.Head>
       <Table.Body>
-        {movies.map(movie => (
-          <Table.Row key={movie.movie_id}>
-            <Table.TextCell>{movie.title}</Table.TextCell>
-            <Table.TextCell isNumber>{movie.movie_id}</Table.TextCell>
-            <Table.TextCell isNumber>{movie.score.toFixed(4)}</Table.TextCell>
+        {rows.map(row => (
+          <Table.Row key={row.id}>
+            <Table.TextCell>{row.title}</Table.TextCell>
+            <Table.TextCell isNumber>{row.id}</Table.TextCell>
+            <Table.TextCell isNumber>{row.score.toFixed(4)}</Table.TextCell>
           </Table.Row>
         ))}
       </Table.Body>
     </Table>
-  </Fragment>
+  </>
+)
+
+const MovieTable = movies => (
+  <TableTemplate
+    heading='Movies'
+    headerText={['Title', 'ID', 'Score']}
+    rows={movies.map(m => ({ title: m.title, id: m.movie_id, score: m.score }))}
+  />
 )
 
 const UserTable = users => (
-  <Fragment>
-    <Heading align='center' marginBottom={16}>
-      Users
-    </Heading>
-    <Table>
-      <Table.Head>
-        <Table.TextHeaderCell>Name</Table.TextHeaderCell>
-        <Table.TextHeaderCell>ID</Table.TextHeaderCell>
-        <Table.TextHeaderCell>Simularity</Table.TextHeaderCell>
-      </Table.Head>
-      <Table.Body>
-        {users.map(user => (
-          <Table.Row key={user.user_id}>
-            <Table.TextCell>{user.name}</Table.TextCell>
-            <Table.TextCell isNumber>{user.user_id}</Table.TextCell>
-            <Table.TextCell isNumber>
-              {user.simularity.toFixed(4)}
-            </Table.TextCell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
-  </Fragment>
+  <TableTemplate
+    heading='Users'
+    headerText={['Name', 'ID', 'Simularity']}
+    rows={users.map(u => ({
+      title: u.name,
+      id: u.user_id,
+      score: u.simularity
+    }))}
+  />
 )
 
 const RecommendationTable = ({ recommendations }) => (
