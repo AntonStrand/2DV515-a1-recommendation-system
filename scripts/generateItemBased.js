e3f06f62e5dfe93c50a4f44d9c3d2e6a16cbb336
Anton Strand <jag@antonstrand.se>
2019-11-18 09:23:14 +0100

    Fix skewed item-based recommendations bug

--- scripts/generateItemBased.js
+++ scripts/generateItemBased.js
@@ -59,16 +59,17 @@ const groupRatingsAsMovies = ratings =>
 /** euclidean :: ([Rating], [Rating]) -> Number */
 const euclidean = (as, bs) => {
   let sim = 0
-
+  let matches = 0
   as.forEach(a => {
     bs.forEach(b => {
       if (a.user_id === b.user_id) {
         sim += (a.rating - b.rating) ** 2
+        matches++
       }
     })
   })
   // don't invert the value if there is no match
-  return sim > 0 ? 1 / (1 + sim) : 0
+  return matches > 0 ? 1 / (1 + sim) : 0
 }
 
 /** *** MAIN *** **/
